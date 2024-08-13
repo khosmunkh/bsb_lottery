78d3750edf334d8b3abc5ab280282d4769c12b81
tsogtgerel_bsb <tsogtgerel@bsb.mn>
2024-08-13 19:38:09 +0800

    initialize 2

---
+++ Dockerfile
@@ -0,0 +1,17 @@
+FROM python:3
+ENV PYTHONUNBUFFERED 1
+RUN pip install --upgrade pip
+
+COPY ./requirements.txt .
+RUN pip install -r requirements.txt
+
+COPY ./bsb_margaash /app/bsb_margaash/
+COPY ./main /app/main
+COPY ./manage.py /app/
+COPY ./.env /app/
+
+
+WORKDIR /app
+
+COPY ./entrypoint.sh /
+ENTRYPOINT ["sh", "/entrypoint.sh"]
--- bsb_lottery/settings.py
+++ bsb_lottery/settings.py
@@ -77,8 +77,12 @@ WSGI_APPLICATION = 'bsb_lottery.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': os.getenv('DB_NAME', 'bsb_margaash'),
+        'USER': 'postgres',
+        'PASSWORD': os.getenv('DB_PASSWORD', '123'),
+        'HOST': os.getenv('DB_HOST', 'localhost'),
+        'PORT': os.getenv('DB_PORT', '5432'),
     }
 }
 
