86eb5adef65451bee877403a10898f2f1c543066
tsogtgerel_bsb <tsogtgerel@bsb.mn>
2024-08-16 17:18:03 +0800

    Edits

--- bsb_lottery/settings.py
+++ bsb_lottery/settings.py
@@ -121,9 +121,9 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/5.0/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'mn-MN'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'Asia/Ulaanbaatar'
 
 USE_I18N = True
 
